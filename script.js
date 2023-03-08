7d94ce4be4efcc4bba6f4df3b6e11d3a1c2ea6d2
Hassan Abdinur <hassan.abdinur123@gmail.com>
2023-03-07 17:46:37 -0800

    added h3 content

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-function setGradient() {}
-
-color1.addEventListener("input", function () {
+const setGradient = () => {
   body.style.background =
     "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
-});
+  css.textContent = body.style.background + ";";
+};
 
-color2.addEventListener("input", function () {
-  body.style.background =
-    "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
-});
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
